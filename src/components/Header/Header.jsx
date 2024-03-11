b2c35c9e88efcd77295aa3c40f3245ae041ec629
DianaKryzhanivska <di_parkhomenko@ukr.net>
2024-03-11 14:42:59 +0200

    sprite.svg

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import sprite from "../../images/sprite.svg";
+import { LogoBox } from "./Header.styled";
 
 const Header = () => {
   return (
     <>
-      <p>VocabBuilder</p>
+      <LogoBox>
+        <svg width={36} height={36}>
+          <use href={`${sprite}#logo`} />
+        </svg>
+        <p>VocabBuilder</p>
+      </LogoBox>
     </>
   );
 };
