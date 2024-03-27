fdc42d47868dfd92623047e045fef1cbff84c021
DianaKryzhanivska <di_parkhomenko@ukr.net>
2024-03-27 22:57:20 +0200

    Training page tablet

--- src/components/Training/Training.jsx
+++ src/components/Training/Training.jsx
@@ -4,6 +4,7 @@ import {
   BtnBox,
   CancelBtn,
   Container,
+  FormWrapper,
   InputBox,
   LabelBox,
   NextBtn,
@@ -17,38 +18,40 @@ const Training = () => {
   return (
     <>
       <Container>
-        <form>
-          <Wrapper>
-            <InputBox>
-              <input type="text" name="ua" placeholder="Введіть переклад" />
-              <LabelBox>
-                <svg>
-                  <use href={`${sprite}#ua`} />
-                </svg>
-                <label htmlFor="ua">Ukrainian</label>
-              </LabelBox>
-              <NextBtn type="button">
-                <p>Next</p>
-                <svg>
-                  <use href={`${sprite}#arrow-right`} />
-                </svg>
-              </NextBtn>
-            </InputBox>
-            <Box>
-              <Word>Word</Word>
-              <LabelBox>
-                <svg>
-                  <use href={`${sprite}#en`} />
-                </svg>
-                <label htmlFor="ua">English</label>
-              </LabelBox>
-            </Box>
-          </Wrapper>
-          <BtnBox>
-            <SubmitBtn type="submit">Save</SubmitBtn>
-            <CancelBtn type="button">Cancel</CancelBtn>
-          </BtnBox>
-        </form>
+        <FormWrapper>
+          <form>
+            <Wrapper>
+              <InputBox>
+                <input type="text" name="ua" placeholder="Введіть переклад" />
+                <LabelBox>
+                  <svg>
+                    <use href={`${sprite}#ua`} />
+                  </svg>
+                  <label htmlFor="ua">Ukrainian</label>
+                </LabelBox>
+                <NextBtn type="button">
+                  <p>Next</p>
+                  <svg>
+                    <use href={`${sprite}#arrow-right`} />
+                  </svg>
+                </NextBtn>
+              </InputBox>
+              <Box>
+                <Word>Word</Word>
+                <LabelBox>
+                  <svg>
+                    <use href={`${sprite}#en`} />
+                  </svg>
+                  <label htmlFor="ua">English</label>
+                </LabelBox>
+              </Box>
+            </Wrapper>
+            <BtnBox>
+              <SubmitBtn type="submit">Save</SubmitBtn>
+              <CancelBtn type="button">Cancel</CancelBtn>
+            </BtnBox>
+          </form>
+        </FormWrapper>
       </Container>
     </>
   );
--- src/components/Training/Training.styled.js
+++ src/components/Training/Training.styled.js
@@ -5,10 +5,26 @@ export const Container = styled(CommonContainer)`
   padding-top: 24px;
   padding-bottom: 76px;
   background: ${({ theme }) => theme.colors.background};
+
+  @media only screen and (min-width: 768px) {
+    padding-top: 62px;
+    padding-bottom: 104px;
+  }
+`;
+
+export const FormWrapper = styled.div`
+  @media only screen and (min-width: 768px) {
+    padding: 18px;
+    background: #fff;
+  }
 `;
 
 export const Wrapper = styled.div`
   margin-bottom: 116px;
+
+  @media only screen and (min-width: 768px) {
+    margin-bottom: 40px;
+  }
 `;
 
 export const InputBox = styled.div`
@@ -21,7 +37,15 @@ export const InputBox = styled.div`
     box-sizing: border-box;
     border-radius: 8px 8px 0px 0px;
     border: none;
+    background: ${({ theme }) => theme.colors.white};
     border-bottom: 1px solid ${({ theme }) => theme.colors.tableBorder};
+  }
+
+  @media only screen and (min-width: 768px) {
+    & input {
+      width: 668px;
+      height: 282px;
+    }
   }
 `;
 
@@ -42,6 +66,20 @@ export const LabelBox = styled.div`
     color: ${({ theme }) => theme.colors.black};
     font-size: 14px;
     font-weight: 500;
+  }
+
+  @media only screen and (min-width: 768px) {
+    bottom: 228px;
+
+    & svg {
+      width: 32px;
+      height: 32px;
+    }
+
+    & label {
+      font-size: 16px;
+      line-height: 1.5em;
+    }
   }
 `;
 
@@ -75,6 +113,11 @@ export const Box = styled.div`
   box-sizing: border-box;
   border-radius: 0px 0px 8px 8px;
   background: ${({ theme }) => theme.colors.white};
+
+  @media only screen and (min-width: 768px) {
+    width: 668px;
+    height: 282px;
+  }
 `;
 
 export const Word = styled.p`
@@ -85,6 +128,10 @@ export const Word = styled.p`
   font-size: 16px;
   font-weight: 500;
   line-height: 1.5em;
+
+  @media only screen and (min-width: 768px) {
+    font-size: 20px;
+  }
 `;
 
 export const BtnBox = styled.div`
@@ -92,6 +139,12 @@ export const BtnBox = styled.div`
   flex-direction: column;
   gap: 8px;
   justify-content: center;
+
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+    gap: 10px;
+    justify-content: flex-start;
+  }
 `;
 
 export const SubmitBtn = styled.button`
@@ -102,6 +155,11 @@ export const SubmitBtn = styled.button`
   font-size: 16px;
   font-weight: 700;
   line-height: 1.5em;
+
+  @media only screen and (min-width: 768px) {
+    padding: 14px 80px;
+    font-size: 18px;
+  }
 `;
 
 export const CancelBtn = styled.button`
@@ -110,4 +168,13 @@ export const CancelBtn = styled.button`
   font-size: 16px;
   font-weight: 700;
   text-align: center;
+
+  @media only screen and (min-width: 768px) {
+    padding: 14px 71px;
+    border-radius: 30px;
+    border: 1px solid ${({ theme }) => theme.colors.green};
+    line-height: 1.5em;
+    font-size: 18px;
+    color: ${({ theme }) => theme.colors.green};
+  }
 `;
